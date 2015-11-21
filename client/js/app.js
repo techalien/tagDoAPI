75ef2a70decfa8a59ea10f2e7311c33427abcc2e
Karan Sharma <karansharma1295@gmail.com>
2015-11-21 17:25:29 +0530

    todos complete

--- client/js/app.js
+++ client/js/app.js
@@ -1,32 +1,16 @@
-/*global angular */
+angular
+  .module('app', [
+    'lbServices',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
+      $urlRouterProvider) {
+    $stateProvider
+      .state('todo', {
+        url: '',
+        templateUrl: 'views/todo.html',
+        controller: 'TodoController'
+      });
 
-/**
- * The main TodoMVC app module
- *
- * @type {angular.Module}
- */
-angular.module('todomvc', ['ngRoute', 'ngResource'])
-	.config(function ($routeProvider) {
-		'use strict';
-
-		var routeConfig = {
-			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html',
-			resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
-			}
-		};
-
-		$routeProvider
-			.when('/', routeConfig)
-			.when('/:status', routeConfig)
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+    $urlRouterProvider.otherwise('todo');
+  }]);
